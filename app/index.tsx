0a1a00890bfc73b420a311c4c26dc50680ac9b2b
IveeDev <iviidev899@gmail.com>
2025-09-03 21:39:50 +0100

    Initial commit

---
+++ app/index.tsx
@@ -0,0 +1,85 @@
+import { images, offers } from "@/constants";
+import cn from "clsx";
+import {
+  FlatList,
+  Image,
+  Pressable,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+export default function Index() {
+  return (
+    <SafeAreaView className="flex-1 bg-white">
+      <FlatList
+        data={offers}
+        renderItem={({ item, index }) => {
+          const isCardEven: boolean = index % 2 === 0;
+          return (
+            <View>
+              <Pressable
+                className={cn(
+                  "offer-card",
+                  isCardEven ? "flex-row-reverse" : "flex-row"
+                )}
+                style={{ backgroundColor: item.color }}
+                android_ripple={{ color: "#fffff22" }}
+              >
+                {({ pressed }) => (
+                  <>
+                    <View className="w-1/2 h-full">
+                      <Image
+                        source={item.image}
+                        className="size-full"
+                        resizeMode="contain"
+                      />
+                    </View>
+
+                    <View
+                      className={cn(
+                        "offer-card__info",
+                        isCardEven ? "pl-10" : "pr-5"
+                      )}
+                    >
+                      <Text className="leading-tight h1-bold text-white-100 font-quicksand-bold">
+                        {item.title}
+                      </Text>
+                      <Image
+                        source={images.arrowRight}
+                        className="size-10"
+                        resizeMode="contain"
+                        tintColor="#ffffff"
+                      />
+                    </View>
+                  </>
+                )}
+              </Pressable>
+            </View>
+          );
+        }}
+        contentContainerClassName="pb-28 px-5"
+        ListHeaderComponent={() => (
+          <View className="flex-row w-full px-5 my-5 flex-between">
+            <View className="flex-start">
+              <Text className="small-bold">Deliver To</Text>
+              <TouchableOpacity className="flex-row flex-center gap-x-1 mt-0.5">
+                <Text className="paragraph-bold text-dark-100">
+                  Lagos, Nigeria
+                </Text>
+                <Image
+                  source={images.arrowDown}
+                  className="size-3"
+                  resizeMode="contain"
+                />
+              </TouchableOpacity>
+            </View>
+
+            <Text>Cart</Text>
+          </View>
+        )}
+      />
+    </SafeAreaView>
+  );
+}
